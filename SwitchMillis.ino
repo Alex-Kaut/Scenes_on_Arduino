396c171111c078950a7d9c57b8c60e5d38554fdd
Alex Kaut <86695572+Alex-Kaut@users.noreply.github.com>
2023-09-13 16:21:48 +0300

    Update SwitchMillis.ino

--- SwitchMillis.ino
+++ SwitchMillis.ino
@@ -3,19 +3,19 @@ void setup() {
 }
 
 void loop() {
-  static uint32_t tmr, period;
-  static byte mode = 0;
-  if (millis() - tmr >= period){
+  static uint32_t tmr, period;      //timer variable
+  static byte mode = 0;             //step
+  if (millis() - tmr >= period){    //timer
     tmr = millis();
-    if (++mode >= 4) mode = 0;
+    if (++mode >= 4) mode = 0;      //moving to the next step by timer
     switch(mode){
-      case 0:
+      case 0:                       //first step
         //do something;
-        period = 1000;
-        break;
-      case 1:
+        period = 1000;              //step execution time, 1000 = 1 second
+        break;                      
+      case 1:                       //sedond step
         //do something;
-        period = 1000;
+        period = 1000;              //then similarly
         break;
       case 2:
         //do something;
